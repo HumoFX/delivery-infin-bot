bf581a7b93d15e165f4ad5a8680220824fae012e
h.otakhanov <h.otakhanov@infinbank.com>
2022-05-20 15:20:10 +0500

    some fix in api

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -26,7 +26,6 @@ async def bot_start(message: types.Message, state: FSMContext):
     await state.reset_state()
     user = await get_user()
     print(user)
-    print(user.contact)
     print(await state.get_state())
     deep_link = message.get_args()
     if not user:
