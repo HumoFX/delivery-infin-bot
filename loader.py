9c0e6a2091f1f1fe6c760a62889992f07185d36d
h.otakhanov <h.otakhanov@infinbank.com>
2022-05-20 15:47:55 +0500

    some fix in api

--- loader.py
+++ loader.py
@@ -5,5 +5,5 @@ from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML, proxy=config.HTTP_PROXY)
 # storage = MemoryStorage()
-storage = RedisStorage2('localhost', 6379, db=3)
+storage = RedisStorage2('localhost', 6379, db=1)
 dp = Dispatcher(bot, storage=storage)
